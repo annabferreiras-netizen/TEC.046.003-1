a93ba5de5c5f333d2a2eaeb0634da564b4739897
Marcos Paulo Alves Borges <annabferreira.s@gmail.com>
2026-04-30 09:06:16 -0300

    .

--- atividade_06/animal.js
+++ atividade_06/animal.js
@@ -22,9 +22,20 @@ class Cachorro extends Animal {
     }
 }
 
+
+class calopsita extends Animal {
+    voar() {
+        console.log(`A ${this.nome} está voando.`)
+    }
+
+}
+let calopsita = new calopsita("Jorge");
 let mia = new Gato("Mia");
 let pandora = new Cachorro("Pandora");
 
+
+Jorge.falar("Piu piu!");
+Jorge.voar();
 mia.falar("Miau!");
 mia.ronronar();
 pandora.falar("Au au!");
@@ -34,44 +45,4 @@ pandora.abanarRabo();
 //cobra,calopsita, ramster, etc...
 //tem que estender Animal e ter um métodopróprio
 
-class Animal {
-    constructor(apelido) {
-        this.nome = apelido;
-        
-    }
-    falar(som) {
-         console.log(som);
-    }
 
-}
-
-class calopsita extends Animal {
-    voar() {
-        console.log(`A ${this.nome} está voando.`)
-    }
-
-}
-
-class cobra extends Animal {
-    rastejar() {
-        console.log(`A ${this.nome} está rastejando.`)
-    }
-}
-
-class hamster extends Animal {
-    correrNaRoda() {
-        console.log(`O ${this.nome} está correndo na roda.`)
-    }
-}
-
-let calopsita = new calopsita("Jorge");
-let cobra = new cobra("Sabrina");
-let hamster = new hamster("Ratatouille");
-
-
-calopsita.falar("Piu piu!");
-calopsita.voar();
-cobra.falar("Sssss!");
-cobra.rastejar();
-hamster.falar("Squeak!");
-hamster.correrNaRoda();
