7e1af9b3d3b7d2402ad7569c69e08ddd88932a53
Anna Beatriz Ferreira dos Santos <annabferreira.s@gmail.com>
2026-05-26 12:16:27 -0300

    RPG

--- atividade_RPG/javascript.js
+++ atividade_RPG/javascript.js
@@ -1,4 +1,4 @@
-class personagem{
+class personagem {
     constructor(nome, titulo, hp, mana, energia) {
         this.nome = nome;
         this.titulo = titulo;
@@ -6,11 +6,38 @@ class personagem{
         this.mana = mana;
         this.energia = energia;
     }
+    hero_atacar(alvo, habilidade) {
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+            alvo.vida = alvo.vida - habilidade.dano;
+            //Debitar Mana
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo
+                this.energia += 50;
+            }
+            // Debitar Energia
+            if (habilidade.energia > 0) {
+                this.energia = 0;
+            }
+        } else {
+            return "Sem mana ou energia";
+        }
+    }
+    boss_atacar(alvo) {
+        if (this.energia >= 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
+    }
+
 }
-class Habilidades{
-    constructor(id, nome, custo, energia) {
+class Habilidades {
+    constructor(id, nome, dano, custo, energia) {
         this.id = id;
         this.nome = nome;
+        this.dano = dano;
         this.custo = custo;
         this.energia = energia;
     }
@@ -26,13 +53,23 @@ document.getElementById("titulo-boss").textContent = boss.titulo;
 //Criar Habilidades
 let containerBtn = document.getElementById("controles");
 let listaHabilidades = [
-new Habilidades(1, "Ataque", 4, 0, 0),
-new Habilidades(2, "Skill", 8, 10, 0),
-new Habilidades(3, "Supremo", 15, 0, 100),
+    new Habilidades(1, "⚔️Ataque", 4, 0, 0),
+    new Habilidades(2, "🪙Skill", 8, 10, 0),
+    new Habilidades(3, "💥Supremo", 15, 0, 100),
 
 ];
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");//<button>
     btn.innerText = hab.nome;
+    btn.classList.add("btn", "btn-outline-warning");
     containerBtn.appendChild(btn);
+    btn.onclick = () => {
+        hero.hero_atacar(boss, hab);
+        atualizarTela();
+    }
 });
+const atualizarTela = () => {
+    document.getElementById("hp-boss").value = boss.vida;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia
+}
